fa6cbace4ec63360c2a1a9f47fde7e9b557e6317
evanbones <74276616+evanbones@users.noreply.github.com>
2026-04-17 16:05:48 -0700

    Test Quark fix

--- common/src/main/java/com/evandev/music_tweaks/mixin/SoundEngineJukeboxMixin.java
+++ common/src/main/java/com/evandev/music_tweaks/mixin/SoundEngineJukeboxMixin.java
@@ -6,6 +6,7 @@ import com.evandev.music_tweaks.config.ModConfig;
 import com.mojang.blaze3d.audio.Channel;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.resources.sounds.TickableSoundInstance;
 import net.minecraft.client.sounds.ChannelAccess;
 import net.minecraft.client.sounds.SoundEngine;
 import net.minecraft.core.BlockPos;
@@ -66,6 +67,7 @@ public abstract class SoundEngineJukeboxMixin {
         if (!ModConfig.get().betterJukeboxes) return;
 
         if (p_sound.getSource() == SoundSource.RECORDS &&
+                !(p_sound instanceof TickableSoundInstance) &&
                 p_sound instanceof AbstractSoundInstanceWrapper modifiedSound) {
 
             modifiedSound.setRelative(true);
