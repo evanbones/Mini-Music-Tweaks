ec06a763b1dd85461b392fc88246b2b04e7bbdac
evanbones <74276616+evanbones@users.noreply.github.com>
2026-08-31 21:43:41 -0700

    1.6.0

--- CHANGELOG-LATEST.md
+++ CHANGELOG-LATEST.md
@@ -1,3 +1,3 @@
-### Fixed
+### Added
 
-- Fixed compatibility with vanilla servers.
+- Backported `does_not_trigger_combat_music` damage type tag and Vercte's `musics.json` changes from 1.21.
---
+++ common/src/main/java/com/evandev/music_tweaks/ModTags.java
@@ -0,0 +1,13 @@
+package com.evandev.music_tweaks;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.damagesource.DamageType;
+
+public class ModTags {
+    public static final TagKey<DamageType> DOES_NOT_TRIGGER_COMBAT_MUSIC = TagKey.create(
+            Registries.DAMAGE_TYPE,
+            new ResourceLocation(Constants.MOD_ID, "does_not_trigger_combat_music")
+    );
+}
